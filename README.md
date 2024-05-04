371dc1dcd12fa6b60ef2b86c20154c7e16a87b91
humayune01 <162161731+humayune01@users.noreply.github.com>
2024-05-05 00:48:25 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Simple Calculator
+ Simple CLI Calculator Project written in TypeScript using Inquirer module.
+
+You can run it with npx command in the **terminal** / **command prompt**:
+
+    npx simple-inquirer-calculator
+
+## Develop yourself
+To develop this command-line app you can watch the making of it here:
+
+[Simple Calculator using Inquirer and Chalk with TypeScript](https://www.youtube.com/watch?v=FlqGIWK4uJE)
+
+## Github Repo
+Here is the github link for source code:
+[Simple Calculator](https://github.com/humayune01/Calculator)
---
+++ main.ts
@@ -0,0 +1,29 @@
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+
+let userInput = await inquirer.prompt([
+    {name: 'first_number', type: 'number', message: 'Enter any number: '},
+    {name: 'second_number', type: 'number', message: 'Enter another number: '},
+    {
+        name: 'operator',
+        type: 'list',
+        message: 'Choose any operation: ',
+        choices: ['Addition', 'Subtraction', 'Multiplication', 'Division']
+    }
+]);
+
+if (userInput.operator === 'Addition') {
+    console.log(chalk.blueBright(`Answer is : ${userInput.first_number + userInput.second_number}`));
+}
+else if (userInput.operator === 'Subtraction') {
+    console.log(chalk.blueBright(`Answer is : ${userInput.first_number - userInput.second_number}`));
+}
+else if (userInput.operator === 'Multiplication') {
+    console.log(chalk.blueBright(`Answer is : ${userInput.first_number * userInput.second_number}`));
+}
+else if (userInput.operator === 'Division') {
+    console.log(chalk.blueBright(`Answer is : ${userInput.first_number / userInput.second_number}`));
+}
+else {
+    console.log(chalk.red('Please select any operation.'));
+}
